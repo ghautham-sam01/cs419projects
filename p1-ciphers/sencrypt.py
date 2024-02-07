49616c7a83979bbb72d9fa79dd4f8593a028f8d9
ghautham-sam01 <ghautham.sam01@gmail.com>
2024-02-07 09:23:53 -0500

    changes to stream cipher

--- p1-ciphers/sencrypt.py
+++ p1-ciphers/sencrypt.py
@@ -2,25 +2,48 @@
 
 import argparse,os
 
+m = 256  # modulus
+a = 1103515245  # multiplier
+c = 12345  # increment
 
-def rand_generator(xO,m,a,c):
-    return a*xO + c % m
+def rand_gen(xO):
+    return (a*xO + c) % m
 
-def encrypt():
-    # Generating a rand number
-    rand = rand_generator()
-    
+def sdbm_hash(password):
+    hash_val = 0
+    for b in password:
+        hash_val = b + (hash_val<<6) + (hash_val<<16) - hash
+    return hash_val
 
-def decrypt():
-    pass
+def gen_ks(seed,len):
+    ks = bytearray()
+    curr_val = seed
+    for _ in range(len):
+        curr_val = rand_gen(curr_val)
+        ks.append(curr_val)
+    return ks
+
+def apply_stream_cipher(password,input,output):
+    seed = sdbm_hash(password)
+    ks = gen_ks(seed,len(input))
+    ciphertext = bytearray()
+    for i in range(len(input)):
+        ciphertext.append(input[i] ^ ks[i])
+    output.write(ciphertext)
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Encrypt text using a stream cipher")
-    parser.add_argument("password", type=str, required=True, help="Specify password")
-    parser.add_argument("plaintextfile", nargs="?", type=argparse.FileType("rb"), default=sys.stdin.buffer, help="Input plaintext file (default: stdin)")
-    parser.add_argument("ciphertextfile", nargs="?", type=argparse.FileType("rb"), default=sys.stdin.buffer, help="Input ciphertext file (default: stdin)")
+    parser = argparse.ArgumentParser(description="Encrypt or decrypt file using a stream cipher")
+    parser.add_argument("password", help="Specify password")
+    parser.add_argument("input_file", help="Input plaintext file (default: stdin)")
+    parser.add_argument("output_file",help="Input ciphertext file (default: stdin)")
     args = parser.parse_args()
-
+    try:
+        with open(args.input_file, "rb") as input_file, open(args.output_file, "wb") as output_file:
+            apply_stream_cipher(args.password.encode(), input_file.read(), output_file)
+    except FileNotFoundError:
+        print("Error: File not found")
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
